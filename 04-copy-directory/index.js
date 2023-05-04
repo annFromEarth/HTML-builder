e96b2fd8bb3b76bb5a38b80e4746b7466fe2a6a2
annFromEarth <dv_anna@mail.ru>
2023-05-04 17:17:15 +0300

    fix: add function copyDir to 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,34 +1,39 @@
 const fs = require('fs');
 const path = require ('path');
-const folderToCopy = '04-copy-directory/files';
-const destinationFolder = '04-copy-directory/files-copy'
 
-let filesArray = [];
+(function copyDir() {
 
-fs.rm(destinationFolder, { force: true, recursive: true}, (err) => {
+  const folderToCopy = '04-copy-directory/files';
+  const destinationFolder = '04-copy-directory/files-copy';
+  let filesArray = [];
+
+  fs.rm(destinationFolder, { force: true, recursive: true}, (err) => {
     if (err) throw err;
     createDir();
-  })
+  });
 
-function createDir() {
+  function createDir() {
     fs.mkdir(destinationFolder, { recursive: true }, (err) => {
-        if (err) throw err;
-        readDir()
-      }); 
-}
+      if (err) throw err;
+      readDir();
+    }); 
+  }
 
-function readDir() {
+  function readDir() {
     fs.readdir(folderToCopy, (err, files) => {
-        if(err){
-          throw err;
-        } else {
-          filesArray=files, copy(filesArray)}});
-}
+      if(err){
+        throw err;
+      } else {
+        filesArray=files, copy(filesArray);}});
+  }
 
-function copy(sourceArray) {
+  function copy(sourceArray) {
     sourceArray.forEach(x=>{
-        fs.copyFile(path.join(folderToCopy,x), path.join(destinationFolder,x), (err,x) => {
-            if (err) throw err;
-            console.log(`file copied`);
-    })
-})}
+      fs.copyFile(path.join(folderToCopy,x), path.join(destinationFolder,x), (err) => {
+        if (err) throw err;
+        console.log('file copied');
+      });
+    });}
+
+})();
+
