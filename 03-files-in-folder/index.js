b6c0d641b9b4845e06ca6a06b009f822f81dba3c
annFromEarth <dv_anna@mail.ru>
2023-05-04 14:36:27 +0300

    feat: 03-files-in-folder completed

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,25 @@
+const fs = require('fs');
+const path = require ('path');
+const targetFolder = 'secret-folder';
+const targetFolderPath = path.join(path.dirname(__filename), targetFolder);
+let filesArray = [];
+
+function getFilesInfo(pathsArray) {
+  pathsArray.forEach(x=>{
+    fs.stat(path.join(targetFolderPath , x), (err, stats) => { 
+      if (err) {
+        throw err;
+      } else {
+        if (!stats.isDirectory()) {console.log(path.parse(x).name +' - '+ path.parse(x).ext.replace('.','') +' - '+ stats.size/1000+'kb');
+        }
+      }
+    });
+  });
+
+}
+
+fs.readdir(targetFolderPath, (err, files) => {
+  if(err){
+    throw err;
+  } else {
+    filesArray=files, getFilesInfo(filesArray);}});
